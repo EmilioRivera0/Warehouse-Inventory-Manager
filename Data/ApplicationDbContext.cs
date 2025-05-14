7901d6807ffe984a5fca112d06f8bef4d5cf50b9
EmilioRivera0 <elcholula619@gmail.com>
2025-05-14 12:42:54 -0600

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Warehouse_Inventory_Manager.Data
+{
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
+    {
+    }
+}
