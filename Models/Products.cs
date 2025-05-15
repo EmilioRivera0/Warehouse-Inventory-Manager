62470d767d1ec6132a6f7fd5e269e5b606e9d9b1
EmilioRivera0 <elcholula619@gmail.com>
2025-05-15 16:32:15 -0600

    Feature: Specified Price decimal precision

--- Models/Products.cs
+++ Models/Products.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 
 namespace Warehouse_Inventory_Manager.Models
 {
@@ -7,6 +8,7 @@ namespace Warehouse_Inventory_Manager.Models
         // By convention the PK should be called Id instead of idProduct
         public int Id { get; set; }
         public string Name { get; set; } = null!;
+        [Precision(16,2)]
         public decimal Price { get; set; }
         public int Stock { get; set; } = 0;
         public int Status { get; set; } = 1; // 1 = active (default), 0 = inactive
